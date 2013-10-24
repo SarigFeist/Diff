5959b072700b7d868a1df2fd8a58c9b6f3d57052
MarieLePanda <girardin.lucas@gmail.com>
2013-10-25 00:56:00 +0200

    Ajout type boolean
    Ajout d'une enum pour le type boolean

---
+++ main.c
@@ -0,0 +1,27 @@
+int main(int argc, char** argv)
+{
+    enum boolean
+    {
+        false = 0,
+        true = 1
+    }typedef boolean;
+
+    {
+        if (2 > argc) //Pas assez de paramètre
+        {
+            printf("Nombre de parametres insuffisants\n");
+        }
+        else if (3 > argc) // Commande sans option
+        {
+
+        }
+        else // Commande avec option
+        {
+
+        }
+    }
+
+    return 0;
+
+
+}
