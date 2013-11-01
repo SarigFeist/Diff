a875520931dacab8042d104ee93f83375d4d6d5c
Stitchou <la_muerte_97216@hotmail.com>
2013-11-01 14:48:29 +0100

    restructuration du main

--- main.c
+++ main.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-//TEST NINJA COMMIT
+
+// Définition des structures globales
+
+typedef enum boolean // création du type booleen
+{
+    false = 0,
+    true = 1
+} boolean;
+
+
+struct option // création de la structure d'option
+{
+}
+;
+
+
+// Déclaration des fonctions
+int **createTable(int nbLin, int nbCol);
+
+
+
+
+// Fonction principale
 int main(int argc, char** argv)
 {
-    enum boolean
-    {
-        false = 0,
-        true = 1
-    }typedef boolean;
-	
-	struct option
-	{
-	}
-	;
+    int **Tab_Compteur_de_ligne,index;
 
-    {
+    Tab_Compteur_de_ligne= createTable(100,100);
+
+
+
+
         if (2 > argc) //Pas assez de paramètre
         {
             printf("Nombre de parametres insuffisants\n");
@@ -31,7 +48,21 @@ int main(int argc, char** argv)
         {
 
         }
-    }
+
 
     return 0;
+}
+
+
+
+
+// Définition des fonctions
+int **createTable(int nbLin, int nbCol)
+{
+    int **tableau = (int **)malloc(sizeof(int*)*nbLin);
+    int *tableau2 = (int *)malloc(sizeof(int)*nbCol*nbLin);
+    for(int i = 0 ; i < nbLin ; i++){
+        tableau[i] = &tableau2[i*nbCol];
+    }
+    return tableau;
 }
